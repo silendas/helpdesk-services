a79e74cc4de1d82281ffd1eeac832f56db6cbe39
silendas <muhammadyasminnulhakim34@gmail.com>
2024-08-09 10:52:40 +0700

    Fix(Service): User Search

--- src/main/java/com/cms/helpdesk/management/users/filter/UserFilter.java
+++ src/main/java/com/cms/helpdesk/management/users/filter/UserFilter.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.domain.Specification;
 
+import com.cms.helpdesk.management.users.model.Employee;
 import com.cms.helpdesk.management.users.model.User;
 
+import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Predicate;
 
 public class UserFilter {
@@ -20,23 +22,24 @@ public class UserFilter {
         };
     }
 
-    public Specification<User> query(String search) {
-        return (root, query, criteriaBuilder) -> {
-            List<Predicate> predicates = new ArrayList<>();
+public Specification<User> query(String search) {
+    return (root, query, criteriaBuilder) -> {
+        List<Predicate> predicates = new ArrayList<>();
 
-            if (search != null && !search.isEmpty()) {
-                String searchPattern = "%" + search.toLowerCase() + "%";
-                Predicate namePredicate = criteriaBuilder.like(criteriaBuilder.lower(root.get("employee").get("name")),
-                        searchPattern);
-                Predicate emailPredicate = criteriaBuilder.like(criteriaBuilder.lower(root.get("email")),
-                        searchPattern);
-                Predicate nipPredicate = criteriaBuilder.like(criteriaBuilder.lower(root.get("nip")), searchPattern);
+        if (search != null && !search.isEmpty()) {
+            String searchPattern = "%" + search.toLowerCase() + "%";
+            
+            Join<User, Employee> employeeJoin = root.join("employee");
 
-                predicates.add(criteriaBuilder.or(namePredicate, emailPredicate, nipPredicate));
-            }
+            Predicate namePredicate = criteriaBuilder.like(criteriaBuilder.lower(employeeJoin.get("name")), searchPattern);
+            Predicate emailPredicate = criteriaBuilder.like(criteriaBuilder.lower(root.get("email")), searchPattern);
+            Predicate nipPredicate = criteriaBuilder.like(criteriaBuilder.lower(employeeJoin.get("nip")), searchPattern);
 
-            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
-        };
-    }
+            predicates.add(criteriaBuilder.or(namePredicate, emailPredicate, nipPredicate));
+        }
+
+        return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+    };
+}
 
 }
